dd1e20aa9cc3e144579ae88bcb04f33637a11a56
JRHonda <justinohonda@gmail.com>
2018-06-24 10:21:12 -0400

    +Second commit test for 2018 Soccer Goodies
    Signed-off-by: JRHonda <justinohonda@gmail.com>

---
+++ app/src/main/java/net/lagunalabs/a2018soccergoodies/MainActivity.kt
@@ -0,0 +1,43 @@
+package net.lagunalabs.a2018soccergoodies
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val gridview: GridView = findViewById(R.id.gridview)
+        gridview.adapter = ImageAdapter(this) // this =?= self
+
+        gridview.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, v, position, id ->
+                    Toast.makeText(this, "$position", Toast.LENGTH_SHORT).show()
+                }
+
+
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
